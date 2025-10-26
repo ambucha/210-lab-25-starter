f19cffc486e9d10ba8a44d7d736d92c268ba25ac
ambucha <ambuchatskiy@gmail.com>
2025-10-26 14:48:30 -0700

    initial p2 (was doing something else) commit

--- main.cpp
+++ main.cpp
@@ -9,6 +9,23 @@
 using namespace std;
 
 int main() {
+    // Start timing
+    auto start = high_resolution_clock::now();
+
+    // Example loop to measure
+    vector<int> numbers;
+    for(int i = 0; i < 1000000; ++i) {
+        numbers.push_back(i);
+    }
+
+    // End timing
+    auto end = high_resolution_clock::now();
+
+    // Calculate duration
+    auto duration = duration_cast<milliseconds>(end - start);
+
+    // Output the duration in milliseconds
+    std::cout << "Time taken: " << duration.count() << " milliseconds\n";
 
     return 0;
 }
